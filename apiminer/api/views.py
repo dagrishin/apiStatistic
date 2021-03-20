a04811542a7c9c1c380e13b9fad4598792bb2c26
dagrishin <grishin-dim@yandex.ru>
2021-03-20 17:35:35 +0500

    'home'

--- apiminer/api/views.py
+++ apiminer/api/views.py
@@ -13,10 +13,19 @@ def index_miner(request):
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.connect((host, port))
-    sock.send(b'summary')
-
-    data = sock.recv(2048)
+    # count gpu
+    sock.send(b'config')
+    count_gpu = int(sock.recv(1048).decode().split(',')[5].split('=')[1])
+    print(count_gpu)
     sock.close()
-    context = {'data': data}
+    context = []
+    for gpu in range(count_gpu):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect((host, port))
+        command = f'gpu|{gpu}'
+        sock.send(command.encode())
+        data = sock.recv(1048).decode().split(',')
+        sock.close()
+        context.append({'temperature': data[7], data[8-9], ))
     return render(request, 'api/api_view.html', context)
 
--- testtst.py
+++ testtst.py
@@ -12,16 +12,19 @@ sock.connect((host, port))
 sock.send(b'config')
 count_gpu = int(sock.recv(1048).decode().split(',')[5].split('=')[1])
 print(count_gpu)
-# sock.close()
+sock.close()
 for gpu in range(count_gpu):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect((host, port))
     command = f'gpu|{gpu}'
     print(command)
     # sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     # sock.connect((host, port))
     sock.send(command.encode())
     pprint(sock.recv(1048).decode().split(','))
+    sock.close()
 
-sock.close()
+
 # context = {'data': data}
 # data = data.
 # pprint(data.decode().split(','))
