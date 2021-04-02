875205b9f883c8632adc679c1bdcb32af841e063
dagrishin <grishin-dim@yandex.ru>
2021-04-02 16:44:30 +0500

    replace informer

--- src/collector/views.py
+++ src/collector/views.py
@@ -1,12 +1,13 @@
 from django.contrib import messages
 from django.contrib.auth.mixins import UserPassesTestMixin, LoginRequiredMixin
-from django.contrib.auth.models import User
+# from django.contrib.auth.models import User
 from django.contrib.messages.views import SuccessMessageMixin
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render
+# from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, ListView, DetailView, FormView, UpdateView, DeleteView
-from django.views.generic.base import View
+from django.views.generic import CreateView, ListView, DetailView, FormView
+from django.views.generic.edit import UpdateView, DeleteView
+# from django.views.generic.base import View
 from django.utils.translation import gettext_lazy as _
 from django.utils import timezone
 
@@ -52,17 +53,17 @@ class InformerGetMixin(object):
             return HttpResponseRedirect('/')
         return super().get(self.request, *args, **kwargs)
 
-    def post(self, *args, **kwargs):
-        informer_id = kwargs['pk']
-        user_informer = None
-        for inform in Informer.objects.filter(pk=informer_id):
-            user_informer = inform.user.id
-        if not user_informer or user_informer != self.request.user.id:
-            messages.success(
-                self.request,
-                _(f'Вы не имеете права редактировать данный контент или контента не существует'))
-            return HttpResponseRedirect('/')
-        return super().get(self.request, *args, **kwargs)
+    # def post(self, *args, **kwargs):
+    #     informer_id = kwargs['pk']
+    #     user_informer = None
+    #     for inform in Informer.objects.filter(pk=informer_id):
+    #         user_informer = inform.user.id
+    #     if not user_informer or user_informer != self.request.user.id:
+    #         messages.success(
+    #             self.request,
+    #             _(f'Вы не имеете права редактировать данный контент или контента не существует'))
+    #         return HttpResponseRedirect('/')
+    #     return super().get(self.request, *args, **kwargs)
 
 
 class UserAuthMixin(UserPassesTestMixin):
@@ -89,13 +90,30 @@ class InformerAllView(LoginRequiredMixin, ListView):
     model = Informer
     template_name = 'collector/informers.html'
 
+    def get_context_data(self, **kwargs):
+        kwargs = super().get_context_data(**kwargs)
+        informers = Informer.objects.filter(user=self.request.user)
+        informers_data = []
+        for informer in informers:
+            info_gpu = get_inform_gpu(host=informer.host, port=informer.port)
+
+            if info_gpu:
+                # add_informer_data(informer_id=informer.id, informer_data=info_gpu)
+                title = informer.title
+                data = {'info_gpu': info_gpu}
+                data['title'] = title
+                data['pk'] = informer.id
+                informers_data.append(data)
+
+        kwargs['informers_data'] = informers_data
+        return kwargs
     def get_queryset(self):
         return self.model.objects.filter(user=self.request.user)
 
 
 class InformerDetailView(InformerGetMixin, LoginRequiredMixin, DetailView):
     model = Informer
-    template_name = 'collector/informer_view.html'
+    template_name = 'collector/informer_detail.html'
 
     def get_context_data(self, **kwargs):
         kwargs = super().get_context_data(**kwargs)
@@ -115,7 +133,7 @@ class InformerDetailView(InformerGetMixin, LoginRequiredMixin, DetailView):
 class CreateInformerView(LoginRequiredMixin, FormView):
     model = Informer
     template_name = 'collector/create_informer.html'
-    success_url = reverse_lazy('collector:all')
+    success_url = reverse_lazy('informer:all')
     form_class = CreateInformerForm
 
     def form_valid(self, form):
@@ -138,16 +156,22 @@ class InformerUpdate(
     LoginRequiredMixin,
     UpdateView):
     """Обновление информера"""
-    success_url = reverse_lazy('collector:all')
-    model = Informer
     template_name = 'collector/create_informer.html'
-    fields = ['title', 'host', 'port']
+    success_url = reverse_lazy('informer:all')
+    model = Informer
+
+    fields = ['title', 'host', 'port', 'timeout']
     success_message = _('информер изменен')
+
+    #
+    # def get_success_url(self):
+    #     messages.success(self.request, self.success_message)
+    #     return self.success_url.format(**self.object.__dict__)
 
 
 class InformerDelete(LoginRequiredMixin, InformerGetMixin, DeleteView):
     """Класс удаления информера"""
-    success_url = reverse_lazy('collector:all')
+    success_url = reverse_lazy('informer:all')
     template_name = 'collector/informer_confirm_delete.html'
     model = Informer
 
